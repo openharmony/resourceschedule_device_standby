8bfe13399db75e9087de306445d4593389c60352
geraltxu <xufanghan@huawei.com>
2023-08-03 00:36:13 +0000

    update services/core/src/standby_service_impl.cpp.
    Signed-off-by: geraltxu <xufanghan@huawei.com>

--- services/core/src/standby_service_impl.cpp
+++ services/core/src/standby_service_impl.cpp
@@ -52,7 +52,7 @@ namespace {
 const std::string ALLOW_RECORD_FILE_PATH = "/data/service/el1/public/device_standby/allow_record";
 const std::string STANDBY_MSG_HANDLER = "StandbyMsgHandler";
 const std::string ON_PLUGIN_REGISTER = "OnPluginRegister";
-#ifdef__LP64__
+#ifdef __LP64__
 const std::string SYSTEM_SO_PATH = "/system/lib64/";
 #else
 const std::string SYSTEM_SO_PATH = "/system/lib/";
