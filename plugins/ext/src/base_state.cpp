203eca8817c3ef20d7c94bf3fad292c32d5f2776
PQBUG <jiahongyu3@h-partners.com>
2025-03-04 01:57:42 +0000

    update plugins/ext/src/base_state.cpp.
    Signed-off-by: PQBUG <jiahongyu3@h-partners.com>

--- plugins/ext/src/base_state.cpp
+++ plugins/ext/src/base_state.cpp
@@ -39,7 +39,11 @@ bool BaseState::runningLockStatus_ = false;
 ErrCode BaseState::Init(const std::shared_ptr<BaseState>& statePtr)
 {
     auto callbackTask = [statePtr]() { statePtr->StartTransitNextState(statePtr); };
+#ifdef STANDBY_WATCH_PRODUCT_ENABLE
+    enterStandbyTimerId_ = TimedTask::CreateTimer(false, 0, 1, callbackTask);
+#else
     enterStandbyTimerId_ = TimedTask::CreateTimer(false, 0, true, false, callbackTask);
+#endif
     if (enterStandbyTimerId_ == 0) {
         STANDBYSERVICE_LOGE("%{public}s state init failed", STATE_NAME_LIST[GetCurState()].c_str());
         return ERR_STANDBY_STATE_INIT_FAILED;
