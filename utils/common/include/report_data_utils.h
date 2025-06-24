861ba6455276f26cb2138fdb0edf370e65b6ab40
redlotus127 <yyflfszs@qq.com>
2025-06-24 04:31:08 +0000

    patch 2
    Signed-off-by: redlotus127 <yyflfszs@qq.com>

---
+++ utils/common/include/report_data_utils.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef STANDBY_REPORT_DATA_UTILS
+#define STANDBY_REPORT_DATA_UTILS
+
+#include <iostream>
+#include "sys/types.h"
+#include "nlohmann/json.hpp"
+
+namespace OHOS {
+namespace DevStandbyMgr {
+using ReportDataFunc = void (*)(uint32_t resType, int64_t value, const nlohmann::json& payload);
+class ReportDataUtils {
+public:
+    ReportDataUtils();
+    ~ReportDataUtils();
+    static ReportDataUtils& GetInstance();
+    void ReportDataInProcess(uint32_t resType, int64_t value, const nlohmann::json& payload);
+private:
+
+    void LoadUtils();
+
+    ReportDataFunc reportFunc_ = nullptr;
+};
+} // namespace DevStandbyMgr
+} // namespace OHOS
+#endif // STANDBY_REPORT_DATA_UTILS
---
+++ utils/common/src/report_data_utils.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "report_data_utils.h"
+
+#include <dlfcn.h>
+#include "standby_service_log.h"
+
+namespace OHOS {
+namespace DevStandbyMgr {
+namespace {
+    const std::string RES_SCHED_SERVICE_SO = "libresschedsvc.z.so";
+}
+
+ReportDataUtils::ReportDataUtils()
+{
+    LoadUtils();
+}
+
+ReportDataUtils::~ReportDataUtils()
+{
+    reportFunc_ = nullptr;
+}
+
+ReportDataUtils& ReportDataUtils::GetInstance()
+{
+    static ReportDataUtils instance;
+    return instance;
+}
+
+void ReportDataUtils::LoadUtils()
+{
+    auto handle = dlopen(RES_SCHED_SERVICE_SO.c_str(), RTLD_NOW);
+    if (!handle) {
+        STANDBYSERVICE_LOGW("%{public}s load %{public}s failed!", __func__, RES_SCHED_SERVICE_SO.c_str());
+        return;
+    }
+
+    reportFunc_ = reinterpret_cast<ReportDataFunc>(dlsym(handle, "ReportDataInProcess"));
+    if (!reportFunc_) {
+        STANDBYSERVICE_LOGW("%{public}s load function:ReportDataInProcess failed!", __func__);
+        dlclose(handle);
+        return;
+    }
+    dlclose(handle);
+}
+
+void ReportDataUtils::ReportDataInProcess(uint32_t resType, int64_t value, const nlohmann::json& payload)
+{
+    if (!reportFunc_) {
+        STANDBYSERVICE_LOGD("%{public}s failed, function nullptr.", __func__);
+        return;
+    }
+    reportFunc_(resType, value, payload);
+}
+
+} // namespace DevStandbyMgr
+} // namespace OHOS
