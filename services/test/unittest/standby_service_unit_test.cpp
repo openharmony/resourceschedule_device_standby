f271206d90c7547a47652ddff03de43674d97d3d
Misguyuan <1610682746@qq.com>
2026-02-27 10:25:20 +0800

    修改用例位置
    Signed-off-by: Misguyuan <1610682746@qq.com>

--- services/test/unittest/standby_service_unit_test.cpp
+++ services/test/unittest/standby_service_unit_test.cpp
@@ -764,6 +764,23 @@ HWMTEST_F(StandbyServiceUnitTest, StandbyServiceUnitTest_025, TestSize.Level1, 2
 }
 
 /**
+ * @tc.name: StandbyServiceUnitTest_067
+ * @tc.desc: test HeartBeatValueChanged.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWMTEST_F(StandbyServiceUnitTest, StandbyServiceUnitTest_067, TestSize.Level1, 20)
+{
+    const std::string tag = "data_test";
+    int32_t timesTamp = 1;
+    StandbyService::GetInstance()->state_ = ServiceRunningState::STATE_RUNNING;
+    EXPECT_EQ(StandbyService::GetInstance()->HeartBeatValueChanged(tag, timesTamp), ERR_OK);
+
+    StandbyService::GetInstance()->state_ = ServiceRunningState::STATE_NOT_START;
+    EXPECT_EQ(StandbyService::GetInstance()->HeartBeatValueChanged(tag, timesTamp), ERR_STANDBY_SYS_NOT_READY);
+}
+
+/**
  * @tc.name: StandbyServiceUnitTest_027
  * @tc.desc: test multithread DayNightSwitchCallback of StandbyService.
  * @tc.type: FUNC
@@ -1481,24 +1498,6 @@ HWTEST_F(StandbyServiceUnitTest, StandbyServiceUnitTest_066, TestSize.Level1)
 
     StandbyService::GetInstance()->state_ = ServiceRunningState::STATE_NOT_START;
     EXPECT_EQ(StandbyService::GetInstance()->PushProxyStateChanged(type, enable), ERR_STANDBY_SYS_NOT_READY);
-}
-
-/**
- * @tc.name: StandbyServiceUnitTest_067
- * @tc.desc: test HeartBeatValueChanged.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(StandbyServiceUnitTest, StandbyServiceUnitTest_067, TestSize.Level1)
-{
-    StandbyServiceImpl::GetInstance()->InitReadyState();
-    StandbyService::GetInstance()->state_ = ServiceRunningState::STATE_RUNNING;
-    const std::string tag = "data_test";
-    int32_t timesTamp = 1;
-    EXPECT_EQ(StandbyService::GetInstance()->HeartBeatValueChanged(tag, timesTamp), ERR_OK);
-
-    StandbyService::GetInstance()->state_ = ServiceRunningState::STATE_NOT_START;
-    EXPECT_EQ(StandbyService::GetInstance()->HeartBeatValueChanged(tag, timesTamp), ERR_STANDBY_SYS_NOT_READY);
 }
 }  // namespace DevStandbyMgr
 }  // namespace OHOS
